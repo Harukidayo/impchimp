9963dce28906720a58b412f27173e6c1ada1dec8
Haruki <haruki@haruki-VirtualBox.(none)>
2013-11-23 23:09:07 -0500

    added maps

---
+++ db/migrate/20131124034645_alter_pizzas_fix_lat_lng.rb
@@ -0,0 +1,8 @@
+class AlterPizzasFixLatLng < ActiveRecord::Migration
+  def change
+  	change_column :pizzas, :lat, :decimal, :precision => 15, :scale => 10
+	change_column :pizzas, :lng, :decimal, :precision => 15, :scale => 10
+  	
+
+  end
+end
